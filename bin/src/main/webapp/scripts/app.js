28fa3a36b5a1b69cf16a3673c1bc1d42944ab3b7
Peruzzi <federico.peruzzi@capgemini.com>
2017-12-07 18:03:53 +0100

    removed bin added to git ignore

--- bin/src/main/webapp/scripts/app.js
+++
@@ -1,6 +0,0 @@
-/**
- * Con questa istruzione andiamo a definire la nostra applicazione angular a cui ci agganciamo per la definizione di directive, 
- * controller e services
- * 
- */
-var myApp = angular.module('filmRental', []);
